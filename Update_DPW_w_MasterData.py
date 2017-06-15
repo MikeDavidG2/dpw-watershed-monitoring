84716d8077ec70b152967700b51597dabcee3345
Mike Grue <MikeDavidG2@yahoo.com>
2017-06-15 11:28:24 -0700

    Added Write_Print_To_Log() function

--- Update_DPW_w_MasterData.py
+++ Update_DPW_w_MasterData.py
@@ -8,21 +8,31 @@
 # Copyright:   (c) mgrue 2017
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-import arcpy
+import arcpy, datetime, os
+
 arcpy.env.overwriteOutput = True
 
 def main():
 
     print 'Starting to run script.\n'
+    #---------------------------------------------------------------------------
+    #                              Set variables
+    #---------------------------------------------------------------------------
 
-    # Set variables
     master_table = r'U:\grue\Scripts\GitHub\DPW-Sci-Monitoring\DEV\Data\DPW_Science_and_Monitoring_prod.gdb\Field_Data'    # Table used to update
     to_update_table = r'X:\day\Testing.mdb\Field_Data'                                                                     # Table to be updated
 
     # TODO: set the log file location, get the print statements to write to the log file
-    log_file = r''
+    run_Write_Print_To_Log = True
+    log_file = r'U:\grue\Scripts\GitHub\DPW-Sci-Monitoring\DEV\Data\Logs\Update_DPW_w_MasterData'
+
     #---------------------------------------------------------------------------
     #                         Start calling Functions()
+    #---------------------------------------------------------------------------
+
+    # Turn the 'print' statement into a logging object
+    if (run_Write_Print_To_Log):
+        orig_stdout = Write_Print_To_Log(log_file)
 
     # Test to make sure there is no existing schema lock
     no_schema_lock = Test_Schema_Lock(to_update_table)
@@ -41,6 +51,89 @@ def main():
     # End of script reporting
     print 'Finished running script.'
 
+    # Return sys.stdout back to its original setting
+    if (run_Write_Print_To_Log):
+
+        # Footer for log file
+        finish_time_str = [datetime.datetime.now().strftime('%m/%d/%Y  %I:%M:%S %p')][0]
+        print '\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        print '                    {}'.format(finish_time_str)
+        print '              Finished Update_DPW_w_MasterData.py'
+        print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+
+        sys.stdout = orig_stdout
+
+        print '\nDone with script.  Please find log file location above for more info.'
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#                          FUNCTION Write_Print_To_Log()
+def Write_Print_To_Log(log_file):
+    """
+    PARAMETERS:
+
+
+    RETURNS:
+
+
+    FUNCTION:
+
+    """
+    print 'Starting Write_Print_To_Log()...'
+
+    # Get the original sys.stdout so it can be returned to normal at the
+    #    end of the script.
+    orig_stdout = sys.stdout
+
+    # Get DateTime to append
+    dt_to_append = Get_DT_To_Append()
+
+    # Create the log file with the datetime appended to the file name
+    log_file_date = '{}_{}.log'.format(log_file,dt_to_append)
+    write_to_log = open(log_file_date, 'w')
+
+    # Make the 'print' statement write to the log file
+    print '  Setting "print" command to write to a log file found at:\n  {}'.format(log_file_date)
+    sys.stdout = write_to_log
+
+    # Header for log file
+    start_time = datetime.datetime.now()
+    start_time_str = [start_time.strftime('%m/%d/%Y  %I:%M:%S %p')][0]
+    print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+    print '                  {}'.format(start_time_str)
+    print '             START Update_DPW_w_MasterData.py'
+    print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n'
+
+    return orig_stdout
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#                          FUNCTION Get_dt_to_append
+def Get_DT_To_Append():
+    """
+    PARAMETERS:
+      none
+
+    RETURNS:
+      dt_to_append (str): Which is in the format 'YYYY_M_D__H_M_S'
+
+    FUNCTION:
+      To get a formatted datetime string that can be used to append to files
+      to keep them unique.
+    """
+    print '  Starting Get_DT_To_Append()...'
+
+    start_time = datetime.datetime.now()
+
+    date = '%s_%s_%s' % (start_time.year, start_time.month, start_time.day)
+    time = '%s_%s_%s' % (start_time.hour, start_time.minute, start_time.second)
+
+    dt_to_append = '%s__%s' % (date, time)
+
+    print '    DateTime to append: {}'.format(dt_to_append)
+
+    print '  Finished Get_DT_To_Append()'
+    return dt_to_append
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
