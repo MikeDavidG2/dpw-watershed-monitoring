aa8c067217bebdeaf36640d06199209e1245ce78
Mike Grue <MikeDavidG2@yahoo.com>
2017-07-05 14:52:31 -0700

    add batch file to call main DPW Sci and Mon script and then the Update DPW script

---
+++ DPW_Sci_and_Mon.bat
@@ -0,0 +1,19 @@
+set jobdir=U:\grue\Scripts\GitHub\DPW-Sci-Monitoring\DEV
+set script_1=%jobdir%\DEV_branch\DPW_Science_and_Monitoring.py
+set script_2=%jobdir%\DEV_branch\Update_DPW_w_MasterData.py
+set log=%jobdir%\Data\Logs\DPW_BatchFile.log
+
+echo -----------------------[START %date% %time%]------------------->>%log%
+
+echo Running %script_1%>>%log%
+Start /wait  %script_1%>>%log%
+
+timeout /T 5
+
+echo Running %script_2%>>%log%
+Start /wait  %script_2%>>%log%
+
+echo -----------------------[END %date% %time%]--------------------->>%log%
+echo ---------------------------------------------------------------------------->>%log%
+echo ---------------------------------------------------------------------------->>%log%
+echo ---------------------------------------------------------------------------->>%log%
