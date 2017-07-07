6ed37080e89705ed89caedf5fb28d2c6ff087032
Mike Grue <MikeDavidG2@yahoo.com>
2017-07-07 07:35:25 -0700

    change name of DPW_Sci_and_Mon.bat to DPW_Science_and_Monitoring.bat

--- DPW_Science_and_Monitoring.bat
+++ DPW_Science_and_Monitoring.bat
@@ -1,17 +1,17 @@
 set jobdir=U:\grue\Scripts\GitHub\DPW-Sci-Monitoring\DEV
 set script_1=%jobdir%\DEV_branch\DPW_Science_and_Monitoring.py
-set script_2=%jobdir%\DEV_branch\Update_DPW_w_MasterData.py
-set log=%jobdir%\Data\Logs\DPW_BatchFile.log
+::set script_2=%jobdir%\DEV_branch\Update_DPW_w_MasterData.py
+set log=%jobdir%\Data\Logs\DPW_Science_and_Monitoring_batch.log
 
 echo -----------------------[START %date% %time%]------------------->>%log%
 
 echo Running %script_1%>>%log%
 Start /wait  %script_1%>>%log%
 
-timeout /T 5
+::timeout /T 5
 
-echo Running %script_2%>>%log%
-Start /wait  %script_2%>>%log%
+::echo Running %script_2%>>%log%
+::Start /wait  %script_2%>>%log%
 
 echo -----------------------[END %date% %time%]--------------------->>%log%
 echo ---------------------------------------------------------------------------->>%log%
