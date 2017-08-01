66abf335739c91f4dea2bf18f566ff1752d20ad5
Mike Grue <MikeDavidG2@yahoo.com>
2017-08-01 15:58:30 -0700

    DPW_Update_sde_load.bat. add ability to call the Backup_FGDB.py in this batch file

--- DPW_Update_sde_load.bat
+++ DPW_Update_sde_load.bat
@@ -1,6 +1,8 @@
+::This Batch file should be run weekly to update sde_load.gdb (Script 1)
+::As well as to create a backup FGDB (Script 2)
 set jobdir=P:\DPW_ScienceAndMonitoring\Scripts\DEV
 set script_1=%jobdir%\DEV_branch\DPW_Update_sde_load.py
-::set script_2=%jobdir%\DEV_branch\Update_DPW_w_MasterData.py
+set script_2=%jobdir%\DEV_branch\Backup_FGDB.py
 set log=%jobdir%\Data\Logs\DPW_Update_sde_load_batch.log
 
 echo -----------------------[START %date% %time%]------------------->>%log%
@@ -8,10 +10,10 @@ echo -----------------------[START %date% %time%]------------------->>%log%
 echo Running %script_1%>>%log%
 Start /wait  %script_1%>>%log%
 
-::timeout /T 5
+timeout /T 5
 
-::echo Running %script_2%>>%log%
-::Start /wait  %script_2%>>%log%
+echo Running %script_2%>>%log%
+Start /wait  %script_2%>>%log%
 
 echo -----------------------[END %date% %time%]--------------------->>%log%
 echo ---------------------------------------------------------------------------->>%log%
